2f467ef205ac4484b156ee445f5288e75e5a3796
shamsham228 <shamishamil228@gmail.com>
2026-05-10 16:46:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = os.getenv("8747572174:AAHvwcb3FINMiPnhpiqXREW4xjLxrsAZH6Q")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found!")
